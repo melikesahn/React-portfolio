cb516ee211c829532b76fed523af3f8dc0612cad
Melike Şahin <melsaahin@gmail.com>
2025-08-11 23:21:09 +0300

    live demo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
             </div>
 
             <form
-                action="https://getform.io/f/formlinki"
+                action="https://getform.io/f/brooqpra"
                 method="POST"
                 className=" max-w-6xl p-5 md:p-12"
                 id="form"
